512c826326cabdbc23a8079bd2bd7e2089906b68
lostwaltz <lostwaltz89@gmail.com>
2026-02-28 10:26:05 +0900

    feat: 로비 HUD 구성 및 설정 패널 정리

--- Assets/Scripts/UI/Panel/Lobby/RcLobbySettingsPresenter.cs
+++ Assets/Scripts/UI/Panel/Lobby/RcLobbySettingsPresenter.cs
@@ -1,58 +1,31 @@
 using Engine.UI;
-using UnityEngine;
 
 namespace Rolice.UI
 {
     public class RcLobbySettingsPresenter : RcUIPresenter<RcLobbySettingsPanel>
     {
+        private bool stageSelectWasOpen;
+
         protected override void OnInitialize()
         {
             Panel.OnCloseClicked += HandleCloseClicked;
-            Panel.OnMasterVolumeChanged += HandleMasterVolumeChanged;
-            Panel.OnSfxVolumeChanged += HandleSfxVolumeChanged;
-            Panel.OnBgmVolumeChanged += HandleBgmVolumeChanged;
-            
-            LoadSettingsData();
+
+            stageSelectWasOpen = RcUIManager.Instance.IsOpen<RcStageSelectPanel>();
+            if (stageSelectWasOpen)
+                RcUIManager.Instance.Close<RcStageSelectPanel>();
         }
 
         protected override void OnDispose()
         {
             Panel.OnCloseClicked -= HandleCloseClicked;
-            Panel.OnMasterVolumeChanged -= HandleMasterVolumeChanged;
-            Panel.OnSfxVolumeChanged -= HandleSfxVolumeChanged;
-            Panel.OnBgmVolumeChanged -= HandleBgmVolumeChanged;
-        }
-
-        private void LoadSettingsData()
-        {
-            // TODO: 실제 계정 시스템 연동 시 수정
-            string mockInfo = "Guest User (Lv.1)";
-            Panel.SetAccountInfo(mockInfo);
-
-            // TODO: RcSoundManager.Instance.GetVolume() 등 실제 데이터 연동
-            Panel.SetMasterVolume(1.0f);
-            Panel.SetSfxVolume(0.8f);
-            Panel.SetBgmVolume(0.6f);
         }
 
         private void HandleCloseClicked()
         {
+            if (stageSelectWasOpen)
+                RcUIManager.Instance.Open<RcStageSelectPanel>();
+
             Panel.Close();
-        }
-
-        private void HandleMasterVolumeChanged(float volume)
-        {
-            // TODO: 사운드 매니저 연동
-        }
-
-        private void HandleSfxVolumeChanged(float volume)
-        {
-            // TODO: 사운드 매니저 연동
-        }
-
-        private void HandleBgmVolumeChanged(float volume)
-        {
-            // TODO: 사운드 매니저 연동
         }
     }
 }
